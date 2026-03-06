2e033ec99277d5ad95b9e9342b7facf0a29f5019
Nguyễn Hoàng Anh <143367993+dfrgtyhujio@users.noreply.github.com>
2026-03-06 15:44:22 +0700

    Add files via upload

---
+++ HN_ENG_KS25CNTT1_NguyenHoangAnh/index.js
@@ -0,0 +1,45 @@
+let users = [] 
+
+function register() {
+    let emailInput = document.getElementById("email");
+    let passwordInput = document.getElementById("password");
+    let rePasswordInput = document.getElementById("rePassword");
+    
+    for (let index = 0; index < users.length; index++) {
+        const element = users[index];
+        if (element.email === emailInput.value) {
+            alert("Email đã được sử dụng.");
+            return;
+        }
+    }
+
+    if (emailInput.value === "" || passwordInput.value === "" || rePasswordInput.value === "") {
+        alert("Không được để trống.");
+        return;
+    }
+
+    if (passwordInput.value.length < 8) {
+        alert("Mật khẩu phải có ít nhất 8 ký tự.");
+        return;
+    }
+
+    if (passwordInput.value !== rePasswordInput.value) {
+        alert("Mật khẩu không khớp.");
+        return;
+    }
+
+    let user = { 
+        email: emailInput.value, 
+        password: passwordInput.value,
+        rePassword: rePasswordInput.value,
+    };
+
+    users.push(user);
+    alert("Đăng ký thành công!");
+    console.log(users);
+
+    emailInput.value = "";
+    passwordInput.value = "";
+    rePasswordInput.value = "";
+
+}
